952dba52669be16f663d3cbfdadbe07decfb4698
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2022-12-08 15:39:16 +0700

    add class Stack and declarate int

--- exe4_085/Program.cs
+++ exe4_085/Program.cs
@@ -6,6 +6,12 @@ using System.Threading.Tasks;
 
 namespace exe4_085
 {
+    class Stack
+    {
+        public string[] arr;
+        public int top;
+        public int max;
+    }
     internal class Program
     {
         static void Main(string[] args)

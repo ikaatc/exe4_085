500217104ab12e0efa984af4eef9042d3ac813a6
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2022-12-08 16:05:11 +0700

    add main menu for program

--- exe4_085/Program.cs
+++ exe4_085/Program.cs
@@ -63,7 +63,19 @@ namespace exe4_085
             Stack s = new Stack();
             while (true)
             {
-                
+                Console.WriteLine();
+                Console.WriteLine("\n***Stack Menu***\n");
+                Console.WriteLine("1. Push");
+                Console.WriteLine("2. Pop");
+                Console.WriteLine("3. Display");
+                Console.WriteLine("4. Exit");
+                Console.WriteLine("\nEnter your choice: ");
+                string sInput = Console.ReadLine();
+                char ch = Convert.ToChar(sInput == "" ? "0" : sInput);
+                switch (ch)
+                {
+
+                }
             }
         }
     }
